3da0deb852d2fe9c8ef83258b2aab9cf72253a30
mwpras0 <mwpras0@gmail.com>
2020-04-06 10:43:44 +0700

    Praktikum 10

---
+++ Praktikum 10/laravelapp/resources/views/profil.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Profil Saya</title>
+</head>
+<body>
+    <h1>Profil Saya</h1>
+    <p>Perkenalkan Nama saya Mohammad Wahyu Prasetyo, saya seorang Mahasiswa POLITEKNIK NEGERI MALANG</p>
+</body>
+</html>
